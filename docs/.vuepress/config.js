a9546f9824a13c7b2aeffbe087f58562c45673e6
webchillgames <web.chill.games@gmail.com>
2024-09-09 22:54:00 +0300

    add base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { viteBundler } from "@vuepress/bundler-vite";
 
 export default defineUserConfig({
   lang: "ru-RU",
-
+  base: "ss-docs",
   title: "Эврика",
   description: "Документация",
 
