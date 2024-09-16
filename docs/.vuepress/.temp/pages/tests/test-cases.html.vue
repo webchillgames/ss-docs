bad32e90d3868304594e52fffe02f4bd24e24569
webchillgames <web.chill.games@gmail.com>
2024-09-16 15:57:51 +0300

    append: тесты

--- docs/.vuepress/.temp/pages/tests/test-cases.html.vue
+++ docs/.vuepress/.temp/pages/tests/test-cases.html.vue
@@ -1,83 +1,151 @@
 <template><div><h1 id="тестирование-системы" tabindex="-1"><a class="header-anchor" href="#тестирование-системы"><span>Тестирование системы</span></a></h1>
 <h2 id="журнал-учителя" tabindex="-1"><a class="header-anchor" href="#журнал-учителя"><span>Журнал учителя</span></a></h2>
+<blockquote>
+<p>Правила пользования таблицей:</p>
+<ol>
+<li>Колонка Тест - то что нужно проверить</li>
+<li>Колонка Готовность - редактируемый раздел. Сделан для удобства отлеживания того, что вы уже протестировали. После обновления страницы - чекбоксы очистятся!</li>
+<li>Колонка Ссылка - ссылки на страницы</li>
+</ol>
+</blockquote>
+<p>Старовые страницы для тестов:</p>
+<ul>
+<li><a href="https://new-school.geekbase.ru/teacher-journal/create" target="_blank" rel="noopener noreferrer">создание журнала</a></li>
+<li><a href="https://new-school.geekbase.ru/subjects-journals" target="_blank" rel="noopener noreferrer">список журналов</a></li>
+<li><a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблица журнала</a></li>
+</ul>
 <table>
 <thead>
 <tr>
-<th>Журнал</th>
+<th>Тест</th>
+<th>Готовность</th>
 </tr>
 </thead>
 <tbody>
 <tr>
-<td>Создание Журнала</td>
+<td>Создание Журнала (только Админ)</td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
 <td>Редактирование Журнала</td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
 <td>Удаление Журнала</td>
+<td><input type="checkbox" /></td>
 </tr>
 </tbody>
 </table>
+<hr>
+<blockquote>
+<p>Для тестов ниже редактируется <a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблица журнала</a> и оцениваются результаты на страницах чьи ссылки указаны в столбце &quot;ссылки&quot;</p>
+</blockquote>
 <table>
 <thead>
 <tr>
-<th>Косвенные разделы</th>
+<th>Тест</th>
+<th>Ссылка</th>
+<th>Готовность</th>
 </tr>
 </thead>
 <tbody>
 <tr>
-<td>Реакция Заметок из Журналов на изменения в таблице Журнала</td>
+<td>Реакция Заметок из Журналов на изменения в  <a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблице журнала</a></td>
+<td><a href="https://new-school.geekbase.ru/journals-comments/list" target="_blank" rel="noopener noreferrer">заметки из журналов</a></td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
-<td>Реакция Дневника на изменение комментария в Журнале</td>
+<td>Реакция Дневника на изменение комментария в  <a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблице журнала</a></td>
+<td><a href="https://new-school.geekbase.ru/diary" target="_blank" rel="noopener noreferrer">дневник</a></td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
-<td>Реакция Дневника на изменение общего комментария в Журнале</td>
-</tr>
-<tr>
-<td>Реакция Отчета Контрольные (Английский) по ребенку</td>
-</tr>
-<tr>
-<td>Реакция Отчета Диктанты (Английский) по ребенку</td>
-</tr>
-<tr>
-<td>Реакция Уровни в динамике</td>
+<td>Реакция Дневника на изменение общего комментария в  <a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблице журнала</a></td>
+<td><a href="https://new-school.geekbase.ru/diary" target="_blank" rel="noopener noreferrer">дневник</a></td>
+<td><input type="checkbox" /></td>
 </tr>
 </tbody>
 </table>
+<h3 id="оценочные-листы" tabindex="-1"><a class="header-anchor" href="#оценочные-листы"><span>Оценочные листы</span></a></h3>
+<blockquote>
+<p>Для тестов ниже нужен Журнал.</p>
+</blockquote>
+<blockquote>
+<p>Из <a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблицы журнала</a> нужно перейти по кнопке &quot;К оценочным&quot; - на страницу со списком оценочных.</p>
+</blockquote>
 <table>
 <thead>
 <tr>
-<th>Оценочные</th>
+<th>Тест</th>
+<th>Готовность</th>
 </tr>
 </thead>
 <tbody>
 <tr>
 <td>Создание Оценочного</td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
 <td>Редактирование Оценочного</td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
 <td>Удаление Оценочного</td>
+<td><input type="checkbox" /></td>
 </tr>
 </tbody>
 </table>
+<blockquote>
+<p>Из <a href="https://new-school.geekbase.ru/teacher-journal/table" target="_blank" rel="noopener noreferrer">таблицы журнала</a> нужно перейти по кнопке &quot;К диктантам&quot; - на страницу со списком диктантов.</p>
+</blockquote>
 <table>
 <thead>
 <tr>
 <th>Диктанты</th>
+<th>Готовность</th>
 </tr>
 </thead>
 <tbody>
 <tr>
 <td>Создание Диктанта</td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
 <td>Редактирование Диктанта</td>
+<td><input type="checkbox" /></td>
 </tr>
 <tr>
 <td>Удаление Диктанта</td>
+<td><input type="checkbox" /></td>
+</tr>
+</tbody>
+</table>
+<blockquote>
+<p>Для тестов ниже редактируются Оценочные листы и оцениваются результаты на страницах чьи ссылки указаны в столбце &quot;ссылки&quot;</p>
+</blockquote>
+<table>
+<thead>
+<tr>
+<th>Тест</th>
+<th>Ссылка</th>
+<th>Готовность</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Реакция Отчета Контрольные (Английский) по ребенку</td>
+<td><a href="https://new-school.geekbase.ru/english-report" target="_blank" rel="noopener noreferrer">Отчет Контрольные</a></td>
+<td><input type="checkbox" /></td>
+</tr>
+<tr>
+<td>Реакция Отчета Диктанты (Английский) по ребенку</td>
+<td><a href="https://new-school.geekbase.ru/english-dictations" target="_blank" rel="noopener noreferrer">Отчет Диктанты</a></td>
+<td><input type="checkbox" /></td>
+</tr>
+<tr>
+<td>Реакция Уровни в динамике на изменения в Оценочных листах ребенка</td>
+<td><a href="https://new-school.geekbase.ru/metrics_by_subject?filterByChildId=332" target="_blank" rel="noopener noreferrer">Уровни в динамике</a></td>
+<td><input type="checkbox" /></td>
 </tr>
 </tbody>
 </table>
